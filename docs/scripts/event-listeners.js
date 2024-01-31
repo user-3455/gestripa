a112412bebb757bd3244256a2b14eecfeb9c345f
user-3455 <158306842+user-3455@users.noreply.github.com>
2024-01-31 16:47:03 +0100

    Add files via upload

---
+++ docs/scripts/event-listeners.js
@@ -0,0 +1,48 @@
+document.getElementById("repair_search1").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+      getALLRepairsByCustomerName();
+    }
+  });
+  
+  document.getElementById("repair_search2").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+      getALLRepairsByBrandName();
+    }
+  });
+    
+  document.getElementById("repair_search3").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        getALLRepairsByCustomerName2();
+    }
+  });
+  
+  document.getElementById("repair_search4").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        getALLRepairsByBrandName2();
+    }
+  });
+
+  document.getElementById("repair_search5").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        getALLRepairsByCustomerName3();
+    }
+  });
+  
+  document.getElementById("repair_search6").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        getALLRepairsByBrandName3();
+    }
+  });
+
+  document.getElementById("repair_search7").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        getALLRepairsByCustomerName4();
+    }
+  });
+  
+  document.getElementById("repair_search8").addEventListener("keyup", function(event) {
+    if (event.key === 'Enter') {
+        getALLRepairsByBrandName4();
+    }
+  });
+  
